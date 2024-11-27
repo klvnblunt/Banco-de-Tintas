adcea9f009fa620e89b10c7fbfaf31a3bd71cd7c
jonasklvn1234@gmail.com <jonasklvn1234@gmail.com>
2024-11-27 16:56:12 -0300

    alterção

--- beneficiario.php
+++ beneficiario.php
@@ -72,6 +72,8 @@
     ?>
    </tbody>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
+  <a href="tela_inicial.php" class="btn btn-primary">Voltar para a tela inicial</a>
 </html>
--- historico_doação.php
+++ historico_doação.php
@@ -67,6 +67,7 @@
     ?>
    </tbody>
     </div>
+    <a href="tela_inicial_adm.php" class="btn btn-primary">Voltar para a tela inicial</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
